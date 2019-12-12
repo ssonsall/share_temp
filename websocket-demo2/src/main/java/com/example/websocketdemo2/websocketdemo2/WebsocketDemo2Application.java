926f25debc97cae157d1d52e4e157bb742522c40
Junhwon Kim <55569886+ssonsall@users.noreply.github.com>
2019-12-12 19:02:20 +0900

    채팅 겨우 돌린거

---
+++ websocket-demo2/src/main/java/com/example/websocketdemo2/websocketdemo2/WebsocketDemo2Application.java
@@ -0,0 +1,13 @@
+//package com.example.websocketdemo2.websocketdemo2;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class WebsocketDemo2Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(WebsocketDemo2Application.class, args);
+//	}
+//
+//}
---
+++ websocket-demo2/src/test/java/com/example/websocketdemo2/websocketdemo2/WebsocketDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketdemo2.websocketdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
